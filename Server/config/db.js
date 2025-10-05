5b0c998ee144176f47992c77911370dc9b8f4629
PodilapuPolinaidu <polinaidupodilapu2@gmail.com>
2025-10-05 11:45:26 +0530

    implemented postgresql

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,18 +1,16 @@
-const mysql = require("mysql2");
+// config/db.js
+const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render PostgreSQL
+  },
 });
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-  } else {
-    console.log("Database connected successfully!");
-    connection.release();
-  }
-});
-module.exports = pool.promise();
+
+pool.connect()
+  .then(() => console.log("PostgreSQL connected successfully"))
+  .catch((err) => console.error("Database connection failed:", err));
+
+module.exports = pool;
