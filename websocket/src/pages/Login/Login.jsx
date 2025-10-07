1df5ec72973c04e033d782cbd3e575f6e326caf1
PodilapuPolinaidu <polinaidupodilapu2@gmail.com>
2025-10-07 22:58:48 +0530

    changed css path

--- websocket/src/pages/Login/Login.jsx
+++ websocket/src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Login.css";
+import "./login.css";
 import axios from "axios";
 import Swal from "sweetalert2";
 import "bootstrap-icons/font/bootstrap-icons.css";
