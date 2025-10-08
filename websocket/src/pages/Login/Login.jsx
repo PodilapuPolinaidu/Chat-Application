5584b996b830f718e82d1cbc8af87a39d683a23a
PodilapuPolinaidu <polinaidupodilapu2@gmail.com>
2025-10-08 16:39:43 +0530

    updated the url

--- websocket/src/pages/Login/Login.jsx
+++ websocket/src/pages/Login/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     }
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/users/login`,
+        "https://chat-application-5-qgda.onrender.com/api/users/login",
         { email, password },
         { withCredentials: true }
       );
